b3c0c2669d34694ce73d7406dd775248277551c5
Michal Charemza <michal@charemza.name>
2020-09-06 10:11:48 +0100

    release: v0.0.6

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def long_description():
 
 setuptools.setup(
     name='asyncio-buffered-pipeline',
-    version='0.0.5',
+    version='0.0.6',
     author='Michal Charemza',
     author_email='michal@charemza.name',
     description='Parallelize pipelines of Python async iterables/generators',
